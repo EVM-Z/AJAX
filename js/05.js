afeb1bbf91a69bc871f59f80cdfb9e0735d00d86
EVM <elyzeo.vm@gmail.com>
2019-12-02 17:07:42 -0600

    Cargando informacion de un archivo externo...

--- js/05.js
+++ js/05.js
@@ -1,2 +1,23 @@
+var btnCargar = document.getElementById('cargar');
 
+function cargarContenidoAjax() {
+    //  Crear nuestro XMLHttpRequest
+    var xhr = new XMLHttpRequest();
 
+    // Abrimos la conexion
+    xhr.open("GET", "texto_prueba.txt", true);
+    xhr.onreadystatechange = function() {
+        console.log(xhr.readyState);
+        //  Verificamos que sea correcta y tenga respuesta
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            console.log("Se cargo correctamente");
+            var contenido = document.getElementById('contenido');
+            contenido.innerHTML = xhr.responseText;
+        }
+
+    }
+
+    xhr.send();
+}
+
+btnCargar.addEventListener('click', cargarContenidoAjax);
